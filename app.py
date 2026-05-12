4ab65b727ddcbd4ba8043beb6af74240c4944aa9
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-05-12 20:41:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ def home():
     return "Γράψε τους αριθμούς τέλος του URL με καθετο , π.χ. /5/4"
 
 @app.route('/<int:n1>/<int:n2>')
-def multiplay(n1,n2):
-    apotelesma = n1*n2
+def sum(n1,n2):
+    apotelesma = n1+n2
     
     return f'''
-        <h1> Υπολογισμός {n1} x {n2} </h1>
+        <h1> Υπολογισμός {n1} + {n2} </h1>
         <p><b>Το αποτέλεσμα είμαι </b>  {apotelesma}</p>    
     '''
 
