796b56fe6407dbfd6260102345ec224410b0c825
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-05-12 20:31:13 +0300

    Create 15thexample.py

---
+++ 15thexample.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Γράψε τους αριθμούς τέλος του URL με καθετο , π.χ. /5/4"
+
+@app.route('/<int:n1>/<int:n2>')
+def multiplay(n1,n2):
+    apotelesma = n1*n2
+    
+    return f'''
+        <h1> Υπολογισμός {n1} x {n2} </h1>
+        <p><b>Το αποτέλεσμα είμαι </b>  {apotelesma}</p>    
+    '''
+
+if __name__ == "__main__":
+    app.run(debug=True)
