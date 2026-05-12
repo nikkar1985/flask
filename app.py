9b8fec36e6bf391c5c7f4734bab4b3f8e377e1aa
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-05-12 20:12:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Γράψε το ποσό σε Ευρώ στο τέλος του URL, π.χ. /50"
+    return "Γράψε μια λέξη στο τέλος του URL, π.χ. /python"
 
-@app.route('/<float:poso>')
-def convert(poso):
-    dolaria = poso * 1.17
-    lires = poso * 0.85
+@app.route('/<keimeno>')
+def transform_text(keimeno):
+    kefalaia = keimeno.upper();
+    mikra = keimeno.lower();
+    mikos = len(keimeno)
     
     return f'''
-        <h1>Μετατροπή για {poso} €</h1>
-        <p><b>Σε Δολάρια:</b> $ {dolaria:.2f}</p>
-        <p><b>Σε Λίρες:</b> £ {lires:.2f}</p>
-        <hr>
-        <p><i>Οι υπολογισμοί έγιναν ζωντανά από την Python!</i></p>
+        <h1>Ανάλυση για {keimeno} €</h1>
+        <p><b>Σε Κεφαλαία</b>  {kefalaia}</p>
+        <p><b>Σε μικρά:</b> {mikra}</p>
+        <p> <b> Αριθμός γραμμάτων : </b> {mikos}</p>
+        
     '''
 
 if __name__ == "__main__":
