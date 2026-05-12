46040d740da5711489da622b478471c59145422b
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-05-12 18:01:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     tora = datetime.datetime.now()
-    ora_format = tora.strftime("%H:%M:$S")
+    ora_format = tora.strftime("%H:%M:%S")
     return f"<h1> Η ώρα είναι {ora_format}</h1>"
 
 if __name__ == "__main__":
