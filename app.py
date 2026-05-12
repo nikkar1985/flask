eeb6ded59aaba2a6028de4e4ececb90580c7a9fd
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-05-12 18:33:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    tautotita = requests.headers.get('User-Agent')
+    tautotita = request.headers.get('User-Agent')
     
             
     return f'''
