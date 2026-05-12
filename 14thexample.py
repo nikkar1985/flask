5a8762ac0b1f18c1801214696aafe7201f81d5de
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-05-12 20:15:09 +0300

    Create 14thexample.py

---
+++ 14thexample.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Γράψε μια λέξη στο τέλος του URL, π.χ. /python"
+
+@app.route('/<keimeno>')
+def transform_text(keimeno):
+    kefalaia = keimeno.upper();
+    mikra = keimeno.lower();
+    mikos = len(keimeno)
+    
+    return f'''
+        <h1>Ανάλυση για {keimeno} </h1>
+        <p><b>Σε Κεφαλαία</b>  {kefalaia}</p>
+        <p><b>Σε μικρά:</b> {mikra}</p>
+        <p> <b> Αριθμός γραμμάτων : </b> {mikos}</p>
+        
+    '''
+
+if __name__ == "__main__":
+    app.run(debug=True)
