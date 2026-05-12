a4452af7164846802b382857307c45671e3e7fe9
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-05-12 20:05:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Γράψε το ποσό σε euro στο τέλος του URL"
+    return "Γράψε το ποσό σε Ευρώ στο τέλος του URL, π.χ. /50"
 
 @app.route('/<float:poso>')
-def check_access(ilikia):
-    dollaria = 1.17 * poso
-    lires = 0.85 * poso
-   
+def convert(poso):
+    dolaria = poso * 1.17
+    lires = poso * 0.85
     
     return f'''
-        <h1 >Μετατροπή για {poso}</h1>
-        <p>Σε δολλάρια {dollaria:.2f} </p>
-        <p> Σε λίρες {lires:.2f} </p>
+        <h1>Μετατροπή για {poso} €</h1>
+        <p><b>Σε Δολάρια:</b> $ {dolaria:.2f}</p>
+        <p><b>Σε Λίρες:</b> £ {lires:.2f}</p>
+        <hr>
+        <p><i>Οι υπολογισμοί έγιναν ζωντανά από την Python!</i></p>
     '''
 
 if __name__ == "__main__":
