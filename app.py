6beb2d16af320cfe579082323d49c7ddcade423e
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-05-12 19:44:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,17 @@ app = Flask(__name__)
 def home():
     return "Γράψε την ηλικία σου στο τέλος του Url πχ 20"
 
-@app.route('/<ilikia>')
-def show_age(ilikia):
+@app.route('/<int:ilikia>')
+def check_access(ilikia):
+    if ilikia >=18:
+        minima : "Είσαι ενήλικας"
+        chroma : "Green "
+    else: 
+        minima : "Είσαι ανήλικος"
+        chroma : "Red "
     return f'''
-        <h1>Είσαι {ilikia} χρονών</h1>
+        <h1 style="color: {chroma};"> {minima} </h1>
+        <p> Δήλωσες ηλικία : {ilikia} </p>
         
     '''
 
