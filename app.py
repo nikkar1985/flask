15cd755b085175463985253c78078394327a918f
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-05-12 17:30:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-app=Flask(__name__)
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return "Είναι η πρώτη μας εφαρμογή " 
+    return "Η εφαρμογή Flask λειτουργεί Maximus!"
 
-if __name__ = "__main__":
-  app.run(debug=True)
-
+if __name__ == "__main__":
+    app.run(debug=True)
