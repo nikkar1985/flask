b1e392200e45ccf4fdc24f7df830163047dcfef8
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-05-12 17:34:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Η εφαρμογή Flask λειτουργεί Maximus!"
+    return "Η εφαρμογή Flask λειτουργεί"
 
 if __name__ == "__main__":
     app.run(debug=True)
