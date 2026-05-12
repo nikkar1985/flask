4f592e95d0ac1c6d1e78038bd01c653315fa29b9
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-05-12 19:25:30 +0300

    Create 11thexample.py

---
+++ 11thexample.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Γράψε την ηλικία σου στο τέλος του Url πχ 20"
+
+@app.route('/<ilikia>')
+def show_age(ilikia):
+    return f'''
+        <h1>Είσαι {ilikia} χρονών</h1>
+        
+    '''
+
+if __name__ == "__main__":
+    app.run(debug=True)
