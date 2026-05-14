ccc4beeee9714697145f343b38a7f1e820b5136a
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-05-14 19:19:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,57 +2,57 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-USERS = [
-    {"name": "nikos", "job": "developer", "color": "#3498db"},
-    {"name": "maria", "job": "designer", "color": "#e74c3c"},
-    {"name": "eleni", "job": "developer", "color": "#2ecc71"},
-    {"name": "kostas", "job": "manager", "color": "#f1c40f"},
-    {"name": "eirini", "job": "manager", "color": "#f1d333"},
-    {"name": "giannis", "job": "teacher", "color": "#d1f403"},
-    {"name": "panagiotis", "job": "teacher", "color": "#123456"}
-
-]
+stats = {
+    "likes":0,
+    "coffee_cups":0
+}
 
 @app.route('/')
 def home():
-    job_filter = request.args.get('job')
-   
-    if job_filter:
-        filtered_users = [u for u in USERS if u['job'] == job_filter.lower()]
-        title = f"Μέλη με επάγγελμα: {job_filter}"
-    else:
-        filtered_users = USERS
-        title = "Όλα τα Μέλη"
+    total_actions = stats["likes"] + stats["coffee_cups"]
+    bg_color="#f1c40f" if stats["likes"]>=10 else '#ffffff'
 
-    user_html = ""
-    for u in filtered_users:
-        user_html += f'''
-            <div style="border-left: 5px solid {u['color']}; margin: 10px; padding: 10px; background: #f4f4f4;">
-                <strong>{u['name'].capitalize()}</strong> - {u['job']}
-                <a href="/user/{u['name']}">Προφίλ</a>
-            </div>
-        '''
+    
 
     return f'''
-    <div style="font-family:sans-serif; max-width:600px; margin:auto;">
-        <h1>{title}</h1>
-        <p>Φιλτράρισμα:
-            <a href="/">Όλοι</a> |
-            <a href="/?job=developer">Developers</a> |
-            <a href="/?job=designer">Designers</a> |
-            <a href="/?job=manager">Manager</a> |
-            <a href="/?job=teacher">Teacher</a>
-        </p>
-        <hr>
-        {user_html if user_html else "<p>Δεν βρέθηκαν μέλη.</p>"}
-    </div>
+    <div style="font-family:sans-serif; text-align:center; padding:50px ; background-color: {bg_color}; min-height:100vh;">
+        <h1>Live Stats Dashboard</h1>
+        <p> Δείξε την υποστήριξή σου στον Developer </p>
+        <div style="diplay: flex; justify-content:center; gap:20px; margin-top:30px;">
+        <div >
+        <h2> Like </h2>
+        <p> {stats["likes"]} </p>
+        <a href="/action/likes"> Like </a>
+        </div>
+        <div >
+        <h2> Coffee Cups </h2>
+        <p> {stats["coffee_cups"]} </p>
+        <a href="/action/coffee_cups"> Coffee Cups </a>
+        </div>
+        </div>
+        <div>
+        <p> Συνολική αλληλεπίδραση {total_actions}
+        <a href='/reset/'> Reset Stats </a>
+        </div>
+        
     '''
 
 
 
-@app.route('/user/<username>')
-def profile(username):
-    return f"<h1>Προφίλ του χρήστη: {username}</h1><a href='/'>Πίσω</a>"
+@app.route('/action/<type')
+def take_action(type):
+    if type in stats:
+        stats[type]+=1
+        return redirect(url_for('home'))
+
+
+@app.route('reset')
+def reset():
+    stats["likes"]=0
+    stats["coffee_cups"]=0
+            return redirect(url_for('home'))
+
+
 
 
 
