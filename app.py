c573499acdf53fe75a03f1962054021bb802c243
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-05-14 17:22:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,33 @@
-import string
-import random
-from flask import Flask, render_template, request, redirect
+from flask import Flask
 
 app = Flask(__name__)
 
-# Εδώ θα αποθηκεύονται τα links σε μορφή {"σύντομο_κλειδί": "μεγάλο_url"}
-# Π.χ. {"abc": "https://www.google.com"}
-url_database = {}
 
-def generate_short_id(length=5):
-    """Παράγει ένα τυχαίο string με γράμματα και αριθμούς (π.χ. 'aB3x9')"""
-    characters = string.ascii_letters + string.digits
-    return ''.join(random.choice(characters) for _ in range(length))
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    short_url = None
-    error = None
+    return '''
+        <div style="text-align: center; font-family:sans-serif; margin-top:50px">
+        <h1> Welcome</h1>
+        <p> Για να δεις το προφίλ σου βάλε στο τέλος του Url το <b>το ονομά σου και το επαγγελμά σου </b> </p>
+        <p> Παράδειγμα : <a href="/Nik/Chessplayer"> Nik/Chessplayer </a></p>
+        </div>
+        '''
 
-    if request.method == 'POST':
-        long_url = request.form.get('long_url')
+
+@app.route('/<onoma>/<epaggelma>')
+def profile(onoma,epaggelma):
+    avatar_url = f"https://robohash.org/{onoma}.png?set=set=4"
+    return '''
+        <div>
+        <img src="{avatar_url}" alt="Avatar">
+        <h1> {onoma}</h1>
+        <p> {epaggelma} </p>
+        <div> Active Profile </div>
+        <a href="/"> Επιστροφή </a>
         
-        # Βασικός έλεγχος αν ο χρήστης έβαλε σωστό link (πρέπει να ξεκινάει με http)
-        if long_url and (long_url.startswith('http://') or long_url.startswith('https://')):
-            # Παράγουμε ένα μοναδικό ID που δεν υπάρχει ήδη στη "βάση" μας
-            short_id = generate_short_id()
-            while short_id in url_database:
-                short_id = generate_short_id()
-            
-            # Αποθήκευση στο dictionary
-            url_database[short_id] = long_url
-            
-            # Φτιάχνουμε το πλήρες short URL για να το δείξουμε στον χρήστη
-            # Το request.host_url μας δίνει αυτόματα το 'http://127.0.0.1:5000/' ή το URL του Render
-            short_url = f"{request.host_url}{short_id}"
-        else:
-            error = "Παρακαλώ βάλε ένα έγκυρο URL που να ξεκινάει με http:// ή https://"
+        '''
 
-    return render_template('index.html', short_url=short_url, error=error)
 
-@app.route('/<short_id>')
-def redirect_to_long(short_id):
-    """Αυτό το route πιάνει οποιοδήποτε κείμενο μετά το '/' (π.χ. /aB3x9)"""
-    # Ψάχνουμε το ID στη "βάση" μας
-    long_url = url_database.get(short_id)
-    
-    if long_url:
-        return redirect(long_url) # Στέλνουμε τον χρήστη στο κανονικό site
-    else:
-        return "<h1>404: Το Short URL δεν βρέθηκε!</h1>", 404
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
