4eb18bcd605bfeeb5ffa9629c315ca69e90c0ed1
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-05-14 17:34:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,34 +2,34 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
+def home():
     return '''
-        <div style="text-align: center; font-family:sans-serif; margin-top:50px">
-        <h1> Welcome</h1>
-        <p> Για να δεις το προφίλ σου βάλε στο τέλος του Url το <b>το ονομά σου και το επαγγελμά σου </b> </p>
-        <p> Παράδειγμα : <a href="/Nik/Chessplayer"> Nik/Chessplayer </a></p>
-        </div>
-        '''
-
+    <div >
+        <h1>Καλωσήρθες στην εφαρμογή Προφίλ!</h1>
+        <p>Για να δεις ένα προφίλ, πρόσθεσε στο τέλος του URL: <b>/το-όνομά-σου/το-επάγγελμά-σου</b></p>
+        <p>Παράδειγμα: <a href="/Nikos/Developer">/Nikos/Developer</a></p>
+    </div>
+    '''
 
 @app.route('/<onoma>/<epaggelma>')
-def profile(onoma,epaggelma):
-    
-        avatar_url = f"https://robohash.org/{onoma}.png?set=set4"
+def profile(onoma, epaggelma):
+    # Αυτό το API δημιουργεί ένα μοναδικό avatar βασισμένο στο όνομα
+    avatar_url = f"https://robohash.org/{onoma}.png?set=set4"
+   
     return f'''
-        <div>
+    <div>  
         <img src="{avatar_url}" alt="Avatar">
-        <h1> {onoma}</h1>
+        <h1 >{onoma}</h1>
         <p> {epaggelma} </p>
-        <div> Active Profile </div>
-        <a href="/"> Επιστροφή </a>
+       
+        <div >
+            Active Profile
         </div>
-        
-        '''
-
-
+       
+        <a href="/" >← Επιστροφή</a>
+    </div>
+    '''
 
 if __name__ == "__main__":
     app.run(debug=True)
